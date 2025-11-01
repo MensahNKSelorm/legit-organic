5e6070bd99f77a19e1cbe680650d68d57440081e
MensahNKSelorm <mensahnkselorm@gmail.com>
2025-11-01 22:35:23 +0100

    Reboot: clean monorepo skeleton

---
+++ README.md
@@ -0,0 +1,2 @@
+# Legit Organic Limited (Reboot)
+Clean monorepo: Angular frontend + Django API backend.
--- legitorganic/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Farm, Certification, Product, BlogPost, ContactMessage
-
-# Register your models here.
-admin.site.register(Farm)
-admin.site.register(Certification)
-admin.site.register(Product)
-admin.site.register(BlogPost)
-admin.site.register(ContactMessage)
